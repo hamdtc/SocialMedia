ceb7b0be386f77761700b80b3cd9f26971254229
hamdtc <mohamedtc73@gmail.com>
2023-06-04 20:17:14 +0530

    check

---
+++ src/Components/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <div style={{ color: "yellow", fontSize: "36px", marginTop: "100px" }}>
+        Loading...
+      </div>
+      <div>click My Media</div>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <span>
+          <Link
+            to="/"
+            className="link"
+            style={{ color: "red", fontSize: "24px" }}
+          >
+            My Media
+          </Link>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
